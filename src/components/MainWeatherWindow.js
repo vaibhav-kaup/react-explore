6113bd478d4022768bb04e65ca95b74e9c5eca3b
Vaibhav Kaup/AAA/Mumbai <vaibhav.kaup@3i-infotech.com>
2026-06-10 14:29:19 +0530

    fix page content

--- src/components/MainWeatherWindow.js
+++ src/components/MainWeatherWindow.js
@@ -9,20 +9,9 @@ export default class MainWeatherWindow extends React.Component {
       <div className='main'>
         <div className='inner-main'>
           {Title}
-          <img
-            src={
-              this.props.data
-                ? require(`../images/${this.props.data.icon}.svg`)
-                : require('../images/01d.svg')
-            }
-            alt='sun'
-            style={{
-              visibility: this.props.city ? 'visible' : 'hidden',
-              opacity: this.props.city ? '1' : '0'
-            }}
-          />
 
-          <div
+        </div>
+        <div
             className='today'
             style={{
               visibility: this.props.city ? 'visible' : 'hidden',
@@ -37,7 +26,6 @@ export default class MainWeatherWindow extends React.Component {
             </p>
             <p>{this.props.data ? this.props.data.weather_desc.toLowerCase() : ''}</p>
           </div>
-        </div>
         {this.props.children}
       </div>
     );
